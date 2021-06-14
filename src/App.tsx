691faf5e9302023088cdd9dea2afa9c87fc4f79a
swapno <swapno5896@yahoo.com>
2021-06-14 02:01:17 -0700

    footer section is almost done

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import MyNavBar from './components/Home/myNavBar/MyNavBar';
 import SmallGllery from './components/Home/SmallGllery/SmallGllery';
 import Events from './components/Home/Events/Events';
+import Footer from './components/Home/Footer/Footer';
 const App = () => {
   return (
     <div>
       <MyNavBar />
       <SmallGllery />
       <Events />
+      <Footer />
     </div>
   );
 };
---
+++ src/components/Home/Footer/Footer.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <div className="container">
+                <div className="row" style={{ backgroundColor: "gray" }}>
+                    <div className="col-md-3">
+                        <h2>This is title</h2>
+                        <p>Designed and built with all the love in the world by the Bootstrap team with the help of our contributors.
+                            Code licensed MIT, docs CC BY 3.0.
+                            Currently v5.0.1.</p>
+
+                    </div>
+                    <div className="col-md-3">
+                        <h2>This is link</h2>
+                        <p>Hallo Eofkd</p>   <p>Hallo Eofkd</p>   <p>Hallo Eofkd</p>   <p>Hallo Eofkd</p>
+                    </div>
+                    <div className="col-md-3">
+                        <h2>Guides</h2>
+                        <p>Hallo Eofkd</p>   <p>Hallo Eofkd</p>   <p>Hallo Eofkd</p>   <p>Hallo Eofkd</p>   <p>Hallo Eofkd</p>
+                    </div>
+                    <div className="col-md-3">
+                        <h2> Important things</h2>
+                        <p>Hallo Eofkd</p>   <p>Hallo Eofkd</p>   <p>Hallo Eofkd</p>   <p>Hallo Eofkd</p>   <p>Hallo Eofkd</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
