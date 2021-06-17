bea8e392ce994815221e54ae90a1562b6ae0b348
swapno <swapno5896@yahoo.com>
2021-06-17 03:07:29 -0700

    d

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import MainHome from '../src/components/Home/MainHome/MainHome'
 import MainEventa from './components/Events/MainEventa/MainEventa';
+import MainGallary from '../src/components/Gallary/MainGallary/MainGallary'
 const App = () => {
   return (
     <div>
       {/* <MainHome /> */}
-      <MainEventa />
+      {/* <MainEventa /> */}
+      <MainGallary />
     </div>
   );
 };
--- src/components/Gallary/MainGallary/MainGallary.js
+++ src/components/Gallary/MainGallary/MainGallary.js
@@ -1,7 +1,46 @@
 import React from "react";
-
+import img1 from "../../../images/m-1.png";
+import GallaryCard from "./../GallaryCard/GallaryCard";
 const MainGallary = () => {
-  return <div></div>;
+  const GallaryData = [
+    {
+      img: img1,
+      title: "ANCIENT STATUES COLLECTIONS",
+      location: "ORGIN: Naples, Italy",
+    },
+    {
+      img: img1,
+      title: "ANCIENT STATUES COLLECTIONS",
+      location: "ORGIN: Naples, Italy",
+    },
+    {
+      img: img1,
+      title: "ANCIENT STATUES COLLECTIONS",
+      location: "ORGIN: Naples, Italy",
+    },
+    {
+      img: img1,
+      title: "ANCIENT STATUES COLLECTIONS",
+      location: "ORGIN: Naples, Italy",
+    },
+    {
+      img: img1,
+      title: "ANCIENT STATUES COLLECTIONS",
+      location: "ORGIN: Naples, Italy",
+    },
+    {
+      img: img1,
+      title: "ANCIENT STATUES COLLECTIONS",
+      location: "ORGIN: Naples, Italy",
+    },
+  ];
+  return (
+    <div className="row">
+      {GallaryData.map((dt) => (
+        <GallaryCard dt={dt} />
+      ))}
+    </div>
+  );
 };
 
 export default MainGallary;
