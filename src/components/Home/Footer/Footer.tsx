665e0a6e287e8555a69e3932e8bc0496fd37dbef
swapno <swapno5896@yahoo.com>
2021-07-02 20:49:05 -0700

    footer is now quite well

--- src/components/Home/Footer/Footer.tsx
+++ src/components/Home/Footer/Footer.tsx
@@ -2,17 +2,17 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div style={{ backgroundColor: "gray", padding: '97px' }}>
+        <div style={{ backgroundColor: "rgba(0,0,0,0.8)", padding: '97px', color: 'white' }}>
             <div className="container">
                 <div className="row">
                     <div className="col-md-3">
-                        <h2>Amazing Museume</h2>
+                        <h2 className='pb-3'>Amazing Museume</h2>
                         <p>Designed and built with all the love in the world by the Bootstrap team with the help of our contributors.
                             Code licensed MIT, docs CC BY 3.0.
                             Currently v5.0.1.</p>
 
                     </div>
-                    <div className="col-md-3">
+                    <div className="col-md-3 ps-5">
                         <h2>This is link</h2>
                         <p>Hallo Eofkd</p>   <p>Hallo Eofkd</p>   <p>Hallo Eofkd</p>   <p>Hallo Eofkd</p>
                     </div>
