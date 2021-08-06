82d0ff17c441c2c803f216fc0f164899ed294c78
swapno5896 <swapno5896@yahoo.com>
2021-08-06 21:45:43 +0600

    home page is done

--- src/components/Events/EventCard/EventCard.js
+++ src/components/Events/EventCard/EventCard.js
@@ -3,16 +3,16 @@ import './EventCard.css'
 const EventCard = (props) => {
   const { img, title, location, time, description } = props.dt;
   return (
-    <div style={{ width: "100%" }} className=" event-cardd shadow p-3 mb-5 bg-white rounded col-md-6 card mb-3 mt-3 ps-3 ">
-      <div className="row g-0">
-        <div className="col-md-4">
+    <div  className=" event-cardd shadow p-3 mb-5 bg-white rounded col-md-6 card mb-3 mt-3 ps-3 ">
+      <div className="row ">
+        <div className="col-md-5">
           <img style={{ width: "100%", height: "100%" }} src={img} alt="..." />
         </div>
-        <div style={{paddingLeft:'60px'}} className="col-md-8 ps-6">
+        <div  className="col-md-7 ps-6">
           <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">{location}</p>
-            <p className="card-text">{time}</p>
+            <h5 className="card-title event-title" >{title}</h5>
+            <p className="event-text">{location}</p>
+            <p className="event-text">{time}</p>
             <p className="card-text">{description}</p>
             <button className='event-book-btn
             p-2'>Book Now</button>
--- src/components/Events/Events/Events.js
+++ src/components/Events/Events/Events.js
@@ -1,5 +1,8 @@
 import React from "react";
-import img1 from "../../../images/m-1.png";
+import img1 from "../../../images/museum-1.png";
+import img2 from "../../../images/museum-2.jpg";
+import img3 from "../../../images/museum-3.jpg";
+import img4 from "../../../images/museum-4.png";
 import EventCard from "../../Events/EventCard/EventCard";
 const Events = () => {
   const eventData = [
@@ -12,7 +15,7 @@ const Events = () => {
         "I have always wanted to have a neighbor just like you. I've always wanted to live in a neighborhood with you",
     },
     {
-      img: img1,
+      img: img2,
       title: "ANCIENT STATUES CASE STUDY",
       location: " Melbourne, Australia",
       time: "Wed 10AM - 12PM",
@@ -20,7 +23,7 @@ const Events = () => {
         "I have always wanted to have a neighbor just like you. I've always wanted to live in a neighborhood with you",
     },
     {
-      img: img1,
+      img: img3,
       title: "LET’S CELEBRATE 25TH MUSEUM DAY",
       location: " Melbourne, Australia",
       time: "Wed 10AM - 12PM",
@@ -28,7 +31,7 @@ const Events = () => {
         "I have always wanted to have a neighbor just like you. I've always wanted to live in a neighborhood with you",
     },
     {
-      img: img1,
+      img: img4,
       title: "LET’S CELEBRATE 25TH MUSEUM DAY",
       location: " Melbourne, Australia",
       time: "Wed 10AM - 12PM",
@@ -37,8 +40,8 @@ const Events = () => {
     },
   ];
   return (
-    <div className="mb-5 pb-5">
-      <h2 className="text-center pb-3">EVENTS</h2>
+    <div className="mt-4 pt-3 mb-5 pb-4">
+      <h2 className="text-center p-3 mt-3">EVENTS</h2>
       <p className="text-center pb-2">
         The movie star the professor and Mary Ann here on Gilligans Isle. Just
         sit right back and you'll hear.
